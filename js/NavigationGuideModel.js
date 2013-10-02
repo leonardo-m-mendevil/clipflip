43550dab795bcce79d24d62929cbf329be4cf01f
leonardo-m-mendevil <eddie.leo.5000@gmail.com>
2013-10-02 12:45:46 -0700

    Create NavigationGuideModel.js

---
+++ js/NavigationGuideModel.js
@@ -0,0 +1,24 @@
+(function(){
+	function NavigationGuideModel(){}
+			 var nGuideModel  = new NavigationGuideModel();
+	             nGuideModel.NavigationGuideModel = function() {
+			                 NavigationGuideModel.prototype.SUBNAV_LAYOUT_SIZE                 = 0;
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_COLOR                = 0x000000;
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_BOLD                 = true;
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_URL                  = '';
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_TARGET               = '_blank';
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_LEFT_MARGIN_OBJECT   = new Object();
+							 NavigationGuideModel.prototype.SUBNAV_LAYOUT_RIGHT_MARGIN_OBJECT  = new Object();
+		         nGuideModel.prototype.function = NavigationGuidModel(s,c,b,u,t,lmo,rmo) {
+					SUBNAV_LAYOUT_SIZE                 = s;
+					SUBNAV_LAYOUT_COLOR                = c;
+					SUBNAV_LAYOUT_BOLD                 = b;
+					SUBNAV_LAYOUT_URL                  = u;
+					SUBNAV_LAYOUT_TARGET               = t;
+					SUBNAV_LAYOUT_LEFT_MARGIN_OBJECT   = lmo;
+					SUBNAV_LAYOUT_RIGHT_MARGIN_OBJECT  = rmo;
+				}
+		
+			}
+	var NavigationGuideModel = NavigationGuideModel || this;
+}
