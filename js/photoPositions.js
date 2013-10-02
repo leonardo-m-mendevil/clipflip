6e3e657baef3c1b2a550b01281a1801b20324143
leonardo-m-mendevil <eddie.leo.5000@gmail.com>
2013-10-02 12:46:13 -0700

    Create photoPositions.js

---
+++ js/photoPositions.js
@@ -0,0 +1,29 @@
+(function(){
+	function photoPositions(){}
+			 var pPositions  = new photoPositions();
+			     pPositions = function() {
+		               this.photoPositions.prototype.all.DEFAULT_SCALE_TO   = 2.9,
+		               this.photoPositions.prototype.all.unit.id.["A1"]     = { scale:2.9,   xPos:6338   ,  yPos:5187  },   
+		               this.photoPositions.prototype.all.unit.id.["A2"]     = { scale:2.9,   xPos:4838   ,  yPos:5187  },   
+		               this.photoPositions.prototype.all.unit.id.["A3"]     = { scale:2.9,   xPos:2338   ,  yPos:5187  },   
+		               this.photoPositions.prototype.all.unit.id.["A4"]     = { scale:2.9,   xPos:-21    ,  yPos:5187  },  
+		               this.photoPositions.prototype.all.unit.id.["A5"]     = { scale:2.9,   xPos:-2248  ,  yPos:5187  },  
+		               this.photoPositions.prototype.all.unit.id.["A6"]     = { scale:2.9,   xPos:-3619  ,  yPos:5187  },  
+		               this.photoPositions.prototype.all.unit.id.["A7"]     = { scale:2.9,   xPos:7500   ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A8"]     = { scale:2.9,   xPos:4398   ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A9"]     = { scale:2.9,   xPos:2827   ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A10"]    = { scale:2.9,   xPos:99     ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A11"]    = { scale:2.9,   xPos:-2227  ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A12"]    = { scale:2.9,   xPos:-3236  ,  yPos:2658  },  
+		               this.photoPositions.prototype.all.unit.id.["A13"]    = { scale:2.9,   xPos:7671   ,  yPos:48    },    
+		               this.photoPositions.prototype.all.unit.id.["A14"]    = { scale:2.9,   xPos:4020   ,  yPos:48    },    
+		               this.photoPositions.prototype.all.unit.id.["A15"]    = { scale:2.9,   xPos:1494   ,  yPos:48    },    
+		               this.photoPositions.prototype.all.unit.id.["A16"]    = { scale:2.9,   xPos:-2039  ,  yPos:48    },    
+		               this.photoPositions.prototype.all.unit.id.["A17"]    = { scale:2.9,   xPos:-3343  ,  yPos:48    },   
+		               this.photoPositions.prototype.all.unit.id.["A18"]    = { scale:2.9,   xPos:6530   ,  yPos:-2464 },
+		               this.photoPositions.prototype.all.unit.id.["A19"]    = { scale:2.9,   xPos:3774   ,  yPos:-2464 },
+		               this.photoPositions.prototype.all.unit.id.["A20"]    = { scale:2.9,   xPos:1237   ,  yPos:-2464 },
+		               this.photoPositions.prototype.all.unit.id.["A21"]    = { scale:2.9,   xPos:-963   ,  yPos:-2464 },
+		               this.photoPositions.prototype.all.unit.id.["A22"]    = { scale:2.9,   xPos:-2166  ,  yPos:-2464 },
+				 }
+}
